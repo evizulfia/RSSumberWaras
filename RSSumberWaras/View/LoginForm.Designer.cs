2fbe265e98f5bca91ca7ddbd06b9681f9fa812b5
Pandu Sunarya <pandusnrya@gmail.com>
2023-01-25 08:29:15 +0700

    add icon

--- RSSumberWaras/View/LoginForm.Designer.cs
+++ RSSumberWaras/View/LoginForm.Designer.cs
@@ -42,9 +42,10 @@
             // loginBtn
             // 
             this.loginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginBtn.Location = new System.Drawing.Point(900, 790);
+            this.loginBtn.Location = new System.Drawing.Point(675, 642);
+            this.loginBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.loginBtn.Name = "loginBtn";
-            this.loginBtn.Size = new System.Drawing.Size(217, 54);
+            this.loginBtn.Size = new System.Drawing.Size(163, 44);
             this.loginBtn.TabIndex = 11;
             this.loginBtn.Text = "Login";
             this.loginBtn.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@
             // 
             this.login.AutoSize = true;
             this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login.Location = new System.Drawing.Point(930, 577);
+            this.login.Location = new System.Drawing.Point(698, 469);
+            this.login.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(118, 46);
+            this.login.Size = new System.Drawing.Size(96, 37);
             this.login.TabIndex = 10;
             this.login.Text = "Login";
             // 
@@ -64,9 +66,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(699, 717);
+            this.label1.Location = new System.Drawing.Point(524, 583);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 25);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "Password";
             // 
@@ -74,18 +77,20 @@
             // 
             this.username.AutoSize = true;
             this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.username.Location = new System.Drawing.Point(695, 655);
+            this.username.Location = new System.Drawing.Point(521, 532);
+            this.username.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(102, 25);
+            this.username.Size = new System.Drawing.Size(83, 20);
             this.username.TabIndex = 8;
             this.username.Text = "Username";
             // 
             // passBox
             // 
             this.passBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passBox.Location = new System.Drawing.Point(834, 712);
+            this.passBox.Location = new System.Drawing.Point(626, 578);
+            this.passBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passBox.Name = "passBox";
-            this.passBox.Size = new System.Drawing.Size(375, 34);
+            this.passBox.Size = new System.Drawing.Size(282, 28);
             this.passBox.TabIndex = 7;
             this.passBox.TextChanged += new System.EventHandler(this.passBox_TextChanged);
             this.passBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.passBox_KeyPress);
@@ -93,17 +98,19 @@
             // unameBox
             // 
             this.unameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.unameBox.Location = new System.Drawing.Point(834, 652);
+            this.unameBox.Location = new System.Drawing.Point(626, 530);
+            this.unameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.unameBox.Name = "unameBox";
-            this.unameBox.Size = new System.Drawing.Size(375, 34);
+            this.unameBox.Size = new System.Drawing.Size(282, 28);
             this.unameBox.TabIndex = 6;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::RSSumberWaras.Properties.Resources.hospital_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(911, 356);
+            this.pictureBox1.Location = new System.Drawing.Point(683, 289);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(172, 158);
+            this.pictureBox1.Size = new System.Drawing.Size(129, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
@@ -111,18 +118,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(908, 517);
+            this.label2.Location = new System.Drawing.Point(681, 420);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(186, 17);
+            this.label2.Size = new System.Drawing.Size(141, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "Rumah Sakit Sumber Waras";
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.ClientSize = new System.Drawing.Size(1426, 839);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.loginBtn);
@@ -131,6 +139,7 @@
             this.Controls.Add(this.username);
             this.Controls.Add(this.passBox);
             this.Controls.Add(this.unameBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginForm";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.maximise);
